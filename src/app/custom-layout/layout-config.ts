ab42c8bdb9a234b17c6514c91557fa6db0f81e8e
Diego Pagini <diegopaginidev@gmail.com>
2021-01-13 16:42:13 -0300

    landingpage

--- src/app/custom-layout/layout-config.ts
+++ src/app/custom-layout/layout-config.ts
@@ -8,5 +8,11 @@ export const CustomConfig: LayoutConfig = {
         //De esta forma estoy definiendo solo los componentes que se van a ver en esta vista: "lg"
       },
     },
+    LandingPage2Template: {
+      lg: {
+        slots: ['Section5'],
+        //Justamente esta pagina no tiene mucho para ver en esta vista, pero quite todo el resto
+      },
+    },
   },
 };
