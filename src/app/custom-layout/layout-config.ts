88319d66e7eb2e21d36bbf5b0607634905b693b7
Diego Pagini <diegopaginidev@gmail.com>
2021-01-13 16:54:41 -0300

    varios componentes en una vista

--- src/app/custom-layout/layout-config.ts
+++ src/app/custom-layout/layout-config.ts
@@ -7,11 +7,25 @@ export const CustomConfig: LayoutConfig = {
         slots: ['SiteLogo', 'SearchBox', 'SiteLogin'],
         //De esta forma estoy definiendo solo los componentes que se van a ver en esta vista: "lg"
       },
+      xs: {
+        slots: ['SearchBox', 'MiniCart'],
+        //En este tamaño solo deje disponible esos dos componentes para que se vean.
+      },
     },
     LandingPage2Template: {
       lg: {
         slots: ['Section5'],
         //Justamente esta pagina no tiene mucho para ver en esta vista, pero quite todo el resto
+      },
+    },
+    ContentPage1Template: {
+      xs: {
+        slots: ['Section2A'],
+        //Deje un solo componente, al parecer tambien reconoce las rutas
+      },
+      lg: {
+        slots: ['Section2a', 'Section2B'],
+        //En la vista "lg": tambien se podria ver ahora la Section2B
       },
     },
   },
